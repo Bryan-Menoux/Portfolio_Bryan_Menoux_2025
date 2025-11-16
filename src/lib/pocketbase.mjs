f4e589a6bfbd721d4161c0bbc33a26d5e8e194f1
Bryan-Menoux <bryan.menoux@edu.univ-fcomte.fr>
2025-11-16 19:02:22 +0100

    Update pocketbase.mjs

--- src/lib/pocketbase.mjs
+++ src/lib/pocketbase.mjs
@@ -1,11 +1,10 @@
 import PocketBase from "pocketbase";
 
-// Détection de l'environnement
+// Détection de l'environnement (client uniquement)
 const isLocal =
-  typeof window !== "undefined"
-    ? window.location.hostname === "localhost" ||
-      window.location.hostname === "127.0.0.1"
-    : process.env.NODE_ENV !== "production";
+  typeof window !== "undefined" &&
+  (window.location.hostname === "localhost" ||
+    window.location.hostname === "127.0.0.1");
 
 // URL de PocketBase
 const baseUrl = isLocal
